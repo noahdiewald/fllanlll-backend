d85a77b480fc41317fd08bdc380342a08a2f54af
Noah Diewald <noah@diewald.me>
2024-09-15 17:55:52 -0400

    supabase recomends setting the search path for functions

---
+++ supabase/migrations/20240915215228_set_search_path_on_functions.sql
@@ -0,0 +1,83 @@
+CREATE OR REPLACE FUNCTION public.set_updated_at_to_now()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+    set search_path = ''
+AS $BODY$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$BODY$;
+
+CREATE OR REPLACE FUNCTION public.write_segment(
+	sj json)
+    RETURNS json
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+    set search_path = ''
+AS $BODY$
+DECLARE
+    segid uuid := (sj ->> 'id');
+    seg_text text := (sj ->> 'segment_text');
+    seg_note text := (sj ->> 'note');
+    seg_trans json := (sj -> 'translations');
+    tr json;
+    traid uuid;
+    tra_text text;
+    newseg json;
+BEGIN
+    IF segid = uuid_nil () THEN
+        INSERT INTO segments (segment_text, note)
+            VALUES (seg_text, seg_note)
+        RETURNING
+            id INTO segid;
+    ELSIF segid IS NOT NULL THEN
+        UPDATE
+            segments
+        SET
+            segment_text = seg_text,
+            note = seg_note
+        WHERE
+            id = segid;
+    ELSE
+        RAISE EXCEPTION 'Invalid segments id %', segid;
+    END IF;
+    DELETE FROM segment_translations
+    WHERE segment_id = segid
+        AND id::text NOT IN (
+            SELECT
+                (ja ->> 'id')
+            FROM
+                json_array_elements(seg_trans) ja);
+    FOR tr IN
+    SELECT
+        json_array_elements(seg_trans)
+        LOOP
+            traid := (tr ->> 'id');
+            tra_text := (tr ->> 'translation_text');
+            IF traid = uuid_nil () THEN
+                INSERT INTO segment_translations (segment_id, translation_text)
+                    VALUES (segid, tra_text);
+            ELSIF traid IS NOT NULL THEN
+                UPDATE
+                    segment_translations
+                SET
+                    translation_text = tra_text
+                WHERE
+                    id = traid;
+            ELSE
+                RAISE EXCEPTION 'Invalid segment_translations id %', traid;
+            END IF;
+        END LOOP;
+    SELECT
+        segment INTO newseg
+    FROM
+        json_segments s
+    WHERE
+        id = segid;
+    RETURN json_object('{segment}', newseg);
+END;
+$BODY$;
